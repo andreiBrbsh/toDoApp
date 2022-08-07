9542363830805884da074025613d3c1faac2fd84
Andrei <97742553+andreiBrbsh@users.noreply.github.com>
2022-08-07 16:54:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const task = document.querySelector('#newThing');
+const addTask = document.querySelector('button');
+const tasks = document.querySelector('#tasks');
+let idCount = 1;
+task.addEventListener('keypress', function(event){
+      if(event.charCode === 13 && task.value.trim() != ''){
+            let checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = `newTasl + ${idCount}`;
+
+            let label = document.createElement('label');
+            label.htmlFor = `newTasl + ${idCount}`;
+            idCount++;
+            label.innerHTML = task.value;
+            task.value = '';
+
+            let br = document.createElement('br');
+
+            tasks.appendChild(checkbox);
+            tasks.appendChild(label);
+            tasks.appendChild(br);
+      } else if(task.value.trim() === ''){
+            task.value = '';
+      }
+})

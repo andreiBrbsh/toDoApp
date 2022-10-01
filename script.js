dbe9656295ad93576c8ddecca844b808c8a80535
andreiBrbsh <andrei.brbsh@icloud.com>
2022-10-01 14:52:17 +0300

    evevnt on button

--- script.js
+++ script.js
@@ -25,3 +25,25 @@ task.addEventListener('keypress', function(event){
             task.value = '';
       }
 });
+
+addTask.addEventListener('click', function(event){
+      if(task.value.trim() != ''){
+            let checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = `newTasl + ${idCount}`;
+
+            let label = document.createElement('label');
+            label.htmlFor = `newTasl + ${idCount}`;
+            idCount++;
+            label.innerHTML = task.value;
+            task.value = '';
+
+            let br = document.createElement('br');
+
+            tasks.appendChild(checkbox);
+            tasks.appendChild(label);
+            tasks.appendChild(br);
+      } else if(task.value.trim() === ''){
+            task.value = '';
+      }
+});
